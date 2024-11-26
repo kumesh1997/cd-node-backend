22a85ec94750f9cb55bc67e4292b5efcf85184da
TharinduKumesh <TharinduKumesh@GRS14844>
2024-11-26 18:36:56 +0530

    Middlewares added

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+
+const errorMiddleware = (req: Request, res: Response, err: Error, next: NextFunction): void => {
+  let statusCode = 500;
+  let errorMessage = "INTERNAL_SERVER_ERROR";
+
+  if (err instanceof CustomError) {
+    statusCode = err.statusCode;
+    errorMessage = err.message;
+  }
+
+  // Send the error response
+  res.status(statusCode).json({ status: false, message: errorMessage, description: err?.message });
+  next();
+};
+
+class CustomError extends Error {
+  constructor(
+    // eslint-disable-next-line no-unused-vars
+    public statusCode: number,
+    message: string
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export { errorMiddleware, CustomError };
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
+import authMiddleware from "../middlewares/auth.middleware";
 import users from "../mockData/users.json";
 
-router.get('/:id', (req, res) => {
+router.get('/:id', authMiddleware, (req, res) => {
     const userId = parseInt(req.params.id);
     const user = users.find(u => u.id==userId)
     res.send(user);
